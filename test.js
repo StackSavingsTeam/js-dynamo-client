fc9c4876a07cd435ff5ec9d438bc3b5a7ce825ce
Hendrix Roa <hendrix.alex87@gmail.com>
2018-01-23 02:16:38 -0400

    agregado test de creado de tablas

---
+++ test.js
@@ -0,0 +1,25 @@
+const assert = require('chai').assert
+const dynamo = require('./dynamoDB')
+
+describe("Tests for DynamoDB", () => {
+	it('Should be create tabla in database', () => {
+		const params = {
+			TableName: 'TestDynamoDB',
+			KeySchema: [
+				{ AttributeName: "fieldTest", "KeyType": "HASH" }
+			],
+			AttributeDefinitions: [
+				{ AttributeName: "fieldTest", "AttributeType": "S" }
+			],
+			ProvisionedThroughput: {
+				"ReadCapacityUnits": 100,
+				"WriteCapacityUnits": 100
+			}
+		}
+		const createTable =  dynamo.createTable(params)
+			createTable.then( res => {
+				assert.equal(true, res.code)
+			}).catch( err => {
+			})		
+	})	
+})
