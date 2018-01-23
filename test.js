5deabeb05f59d0c14dd3d49cfa3cf854b74c8e75
Hendrix Roa <hendrix.alex87@gmail.com>
2018-01-23 02:36:32 -0400

    agregado test para eliminar tabla #25

--- test.js
+++ test.js
@@ -2,24 +2,39 @@ const assert = require('chai').assert
 const dynamo = require('./dynamoDB')
 
 describe("Tests for DynamoDB", () => {
-	it('Should be create tabla in database', () => {
-		const params = {
-			TableName: 'TestDynamoDB',
-			KeySchema: [
-				{ AttributeName: "fieldTest", "KeyType": "HASH" }
-			],
-			AttributeDefinitions: [
-				{ AttributeName: "fieldTest", "AttributeType": "S" }
-			],
-			ProvisionedThroughput: {
-				"ReadCapacityUnits": 100,
-				"WriteCapacityUnits": 100
+
+	before(() => {
+		it('Should be create tabla in database', () => {
+			const params = {
+				TableName: 'TestDynamoDB',
+				KeySchema: [
+					{ AttributeName: "fieldTest", "KeyType": "HASH" }
+				],
+				AttributeDefinitions: [
+					{ AttributeName: "fieldTest", "AttributeType": "S" }
+				],
+				ProvisionedThroughput: {
+					"ReadCapacityUnits": 100,
+					"WriteCapacityUnits": 100
+				}
 			}
+			const createTable =  dynamo.createTable(params)
+				createTable.then( res => {
+					assert.equal(true, res.code)
+				}).catch( err => {
+				})		
+		})
+	})
+
+	after(() => {
+		const param = {
+			TableName: 'TestDynamoDB'
 		}
-		const createTable =  dynamo.createTable(params)
-			createTable.then( res => {
+		const deleteTable =  dynamo.deleteTable(params)
+			deleteTable.then( res => {
 				assert.equal(true, res.code)
 			}).catch( err => {
-			})		
-	})	
+			})
+	})
+		
 })
